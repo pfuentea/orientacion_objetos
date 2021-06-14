d6ee26081fcc31207bca6f9d4307ba9f720ab2b8
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-06-14 19:05:12 -0400

    encadenamiento

--- usuario.py
+++ usuario.py
@@ -6,21 +6,55 @@ class User:
 
     def make_deposit(self,amount):
         self.account_balance+=amount
+        return self
     def make_withdrawl(self,amount):
-        self.account_balance-=amount
+        if self.account_balance<amount:
+            print("Saldo insuficiente")
+        else:
+            self.account_balance-=amount
+        return self
     def display_user_balance (self):
-        print(self.name+' tiene :$'+str(self.account_balance)+' en su cuenta.')
-    def transfer_money (self, other_user, amount):
+        print(self.name+' tiene : $'+str(self.account_balance)+' en su cuenta.')
+        return self
+    def transfer_money(self, other_user, amount):
         self.make_withdrawl(amount)
         other_user.make_deposit(amount)
+        return self
 
-guido = User("Guido van Rossum", "guido@python.com")
-monty = User("Monty Python", "monty@python.com")
-pedro = User("Pedro Picapiedra", "ppicapiedra@bedrock.com")
+ani = User("Anakin Skywalker", "vader@sith.com")
+padme = User("Padmé Amidala", "pamidala@senate_Stellar.com")
+pedro = User("Din Djarin", "mando@mandalor.com")
 
-guido.make_deposit(100)
-guido.make_deposit(200)
-monty.make_deposit(50)
-guido.display_user_balance()
-monty.display_user_balance()
+'''
+ani.make_deposit(100)
+ani.make_deposit(200)
+ani.make_deposit(321)
+ani.make_withdrawl(121)
+ani.display_user_balance()
+'''
+
+ani.make_deposit(100).make_deposit(200).make_deposit(321).make_withdrawl(121).display_user_balance()
+
+'''
+padme.make_deposit(50)
+padme.make_deposit(500)
+padme.make_withdrawl(60)
+padme.display_user_balance()
+'''
+padme.make_deposit(50).make_deposit(500).make_withdrawl(60).display_user_balance()
+
+'''pedro.make_deposit(1000)
+pedro.make_withdrawl(100)
+pedro.make_withdrawl(154)
+pedro.make_withdrawl(238)
+pedro.display_user_balance()
+'''
+pedro.make_deposit(1000).make_withdrawl(100).make_withdrawl(154).make_withdrawl(238).display_user_balance()
+
+ani.transfer_money(pedro,150)
+
+ani.display_user_balance()
+pedro.display_user_balance()
+
+#import pdb; pdb.set_trace()
 
